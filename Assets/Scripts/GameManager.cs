140795e4b44199d016070ba7437a16ca802910bd
Robert B <rbroyles@my.bcit.ca>
2017-10-31 20:52:41 -0700

    Newest Update
    Added UI Panel to hold toggles and timer
    
    Added timer for "scoring"
    
    Added scripts to allow for walking through walls (still not working though so will work more tomorrow)
    
    Added fog shader to project

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
     public Maze maze;
     private Maze thisMaze;
+    public int timer;
+    public Text timerText;
+    public Text survivalTimeText;
     public GameObject player;
     public GameObject enemy;
     public GameObject endPanel;
     private static bool gameEnd = false;
+    
 
     // Use this for initialization
     private void Start()
     {
+        timer = 0;
         endPanel = GameObject.FindGameObjectWithTag("Finish");
         endPanel.SetActive(false);
         BeginGame();
@@ -22,6 +28,7 @@ public class GameManager : MonoBehaviour {
     // Update is called once per frame
     private void Update()
     {
+        timerText.text = timer.ToString();
         if (Input.GetKeyDown(KeyCode.Home) || Input.GetButton("Restart") /*|| Input.touches[0].tapCount == 2*/)
         {
             RestartGame();
@@ -35,6 +42,8 @@ public class GameManager : MonoBehaviour {
         thisMaze.GenerateMaze();
         player.transform.position = new Vector3(9.5f, 0, -9.5f);
         enemy.transform.position = new Vector3(-7.5f, 0, 7.5f);
+        timer = 0;
+        StartCoroutine("SurvivalTime");
     }
 
     private void RestartGame()
@@ -43,15 +52,31 @@ public class GameManager : MonoBehaviour {
         if(gameEnd)
         {
             endPanel.SetActive(false);
+        } else
+        {
+            StopCoroutine("SurvivalTime");
         }        
-
         Destroy(thisMaze.gameObject);
         BeginGame();
     }
 
     public void EndGame()
     {
+        StopCoroutine("SurvivalTime");
         endPanel.SetActive(true);
+        survivalTimeText.text = "You were caught\n"
+            + "You survived " + timer + " seconds\n"
+            + "Try Again";
         gameEnd = true;
+    }
+
+    // Increase the timer by 1 each second passed
+    IEnumerator SurvivalTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timer++;
+        }
     }
 }
---
+++ Assets/Scripts/GhostToggle.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class GhostToggle : MonoBehaviour
+{
+    public GameObject northWall, eastWall, southWall, westWall;
+    BoxCollider north, east, south, west;
+    public Toggle ghostToggle;
+
+    // Use this for initialization
+    void Start()
+    {
+        north = northWall.GetComponent<BoxCollider>();
+        east = eastWall.GetComponent<BoxCollider>();
+        south = southWall.GetComponent<BoxCollider>();
+        west = westWall.GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetButton("GhostToggle"))
+        {
+            //if (ghostToggle.isOn)
+            //{
+                ghostToggle.isOn = !ghostToggle.isOn;
+                north.enabled = !north.enabled;
+                east.enabled = !east.enabled;
+                south.enabled = !south.enabled;
+                west.enabled = !west.enabled;
+           /* }
+            else
+            {
+                ghostToggle.isOn = true;
+                north.isTrigger = !north.isTrigger;
+                east.isTrigger = !east.isTrigger;
+                south.isTrigger = !south.isTrigger;
+                west.isTrigger = !west.isTrigger;
+            }*/
+        }
+    }
+}
